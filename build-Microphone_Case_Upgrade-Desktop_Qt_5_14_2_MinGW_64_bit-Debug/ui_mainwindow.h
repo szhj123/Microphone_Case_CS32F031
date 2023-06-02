2ba4a60f124b8af393656b58760e0d6a97ed4aeb
szhj13 <64463561@qq.com>
2023-06-02 18:03:12 +0800

    debug serial port upgrade tool on Qt

--- build-Microphone_Case_Upgrade-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-Microphone_Case_Upgrade-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -11,13 +11,11 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QFrame>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
-#include <QPainter>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -29,10 +27,9 @@ public:
     QWidget *logoWidget;
     QLabel *labelLogoText;
     QLabel *labelLogoPic;
-
     QPushButton *btnMinimize;
+    QLabel *labelLogoName;
     QPushButton *btnClose;
-    QFrame *line;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -41,39 +38,47 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(640, 400);
+        QFont font;
+        font.setPointSize(10);
+        MainWindow->setFont(font);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         logoWidget = new QWidget(centralwidget);
-        logoWidget->setObjectName(QString::fromUtf8("widget"));
+        logoWidget->setObjectName(QString::fromUtf8("logoWidget"));
         logoWidget->setGeometry(QRect(0, 0, 640, 35));
+        QFont font1;
+        font1.setPointSize(9);
+        logoWidget->setFont(font1);
         labelLogoText = new QLabel(logoWidget);
         labelLogoText->setObjectName(QString::fromUtf8("labelLogoText"));
-        labelLogoText->setGeometry(QRect(80, 2, 261, 31));
-        QFont font;
-        font.setFamily(QString::fromUtf8("MV Boli"));
-        font.setPointSize(14);
-        labelLogoText->setFont(font);
+        labelLogoText->setGeometry(QRect(80, 2, 281, 31));
+        QFont font2;
+        font2.setFamily(QString::fromUtf8("MV Boli"));
+        font2.setPointSize(13);
+        labelLogoText->setFont(font2);
         labelLogoText->setStyleSheet(QString::fromUtf8("color: rgb(50,254,255)"));
         labelLogoPic = new QLabel(logoWidget);
         labelLogoPic->setObjectName(QString::fromUtf8("labelLogoPic"));
         labelLogoPic->setGeometry(QRect(10, 2, 61, 31));
-
         btnMinimize = new QPushButton(logoWidget);
         btnMinimize->setObjectName(QString::fromUtf8("btnMinimize"));
         btnMinimize->setGeometry(QRect(570, 2, 30, 30));
+        labelLogoName = new QLabel(logoWidget);
+        labelLogoName->setObjectName(QString::fromUtf8("labelLogoName"));
+        labelLogoName->setGeometry(QRect(370, 10, 161, 20));
+        QFont font3;
+        font3.setFamily(QString::fromUtf8("Microsoft YaHei UI"));
+        font3.setPointSize(10);
+        labelLogoName->setFont(font3);
+        labelLogoName->setStyleSheet(QString::fromUtf8("color: rgb(255,255,255)"));
+        labelLogoName->setTextFormat(Qt::AutoText);
         btnClose = new QPushButton(logoWidget);
         btnClose->setObjectName(QString::fromUtf8("btnClose"));
         btnClose->setGeometry(QRect(605, 2, 30, 30));
-        line = new QFrame(centralwidget);
-        line->setObjectName(QString::fromUtf8("line"));
-        line->setGeometry(QRect(140, 280, 118, 3));
-        line->setStyleSheet(QString::fromUtf8("color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(255, 0, 0, 255), stop:0.166 rgba(255, 255, 0, 255), stop:0.333 rgba(0, 255, 0, 255), stop:0.5 rgba(0, 255, 255, 255), stop:0.666 rgba(0, 0, 255, 255), stop:0.833 rgba(255, 0, 255, 255), stop:1 rgba(255, 0, 0, 255));"));
-        line->setFrameShape(QFrame::HLine);
-        line->setFrameShadow(QFrame::Sunken);
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 640, 21));
+        menubar->setGeometry(QRect(0, 0, 640, 22));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -90,6 +95,7 @@ public:
         labelLogoText->setText(QCoreApplication::translate("MainWindow", "Microphone Charging Case", nullptr));
         labelLogoPic->setText(QString());
         btnMinimize->setText(QString());
+        labelLogoName->setText(QCoreApplication::translate("MainWindow", "Serial Port Upgrade Tool", nullptr));
         btnClose->setText(QString());
     } // retranslateUi
 
